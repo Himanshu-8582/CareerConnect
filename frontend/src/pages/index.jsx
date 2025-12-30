7d4957fb7610a9d7e8b4120fa65f7267752a01c9
Himanshu <himanshugupta8582@gmail.com>
2025-12-30 15:29:23 +0530

    landing page is completed

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useRouter } from "next/router";
+import UserLayout from "@/layout/UserLayout";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+
+  const router = useRouter();
+  return (
+    <UserLayout>
+      <div className={styles.container}>
+
+        <div className={styles.mainContainer}>
+
+          <div className={styles.mainContainer_left}>
+            <p>Connect with Friends without Exaggeration</p>
+            <p>A true social media platform, with stories no blufs !</p>
+            <div className={styles.buttonJoin} onClick={() => { router.push('/login') }}>
+              <p>Join Now</p>
+            </div>
+          </div>
+
+          <div className={styles.mainContainer_right}>
+            <img src="images/connection.png" alt="" />
+          </div>
+
+        </div>
+
+      </div>
+    </UserLayout>
+  );
+}
---
+++ frontend/src/pages/login/index.jsx
@@ -0,0 +1,11 @@
+
+import UserLayout from '@/layout/UserLayout'
+import React from 'react'
+
+export default function LoginComponent() {
+  return (
+    <UserLayout>
+        Login
+    </UserLayout>
+  )
+}
